4c51c5adaee8f980708ba19f12f84229aedaddb6
Eduardo Marra <eduardomarra@gmail.com>
2022-07-11 22:25:20 -0300

    added Datepicker salesCard component

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,125 +1,145 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import { NotificationButton } from "../NotificationButton";
 import "./styles.css";
 
 export function SalesCard() {
-    return (
-        <>
-            <div className="card">
-                    <h2>Vendas</h2>
-                    <div className="input-form">
-                        <div>
-                            <input type="text" name="" id="" placeholder="01/01/2022"/>
-                        </div>
-                        <div>
-                            <input type="text" name="" id="" placeholder="31/01/2022"/>
-                        </div>
-                    </div>
-                    <div>
-                        <table className="sales-table">
-                            <thead>
-                                <tr>
-                                    <th className="active645">ID</th>
-                                    <th className="mobile">Data</th>
-                                    <th>Vendedor</th>
-                                    <th className="active645">Visitas</th>
-                                    <th className="active645">Vendas</th>
-                                    <th>total</th>
-                                    <th>Notificar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="active645">#001</td>
-                                    <td className="mobile">20/02/2022</td>
-                                    <td>Eduardo Marra</td>
-                                    <td className="active645">20</td>
-                                    <td className="active645">15</td>
-                                    <td>563.543,52</td>
-                                    <td className="notification">
-                                        <NotificationButton />
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-        </>
-    )
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(new Date());
+
+
+  return (
+    <>
+      <div className="card">
+        <h2>Vendas</h2>
+        <div className="input-form">
+          <div>
+            <DatePicker
+              selected={minDate}
+              onChange={(date: Date) => setMinDate(date)}
+              className="dsmeta-form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+          <div>
+            <DatePicker
+              selected={maxDate}
+              onChange={(date: Date) => setMaxDate(date)}
+              className="dsmeta-form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
+        <div>
+          <table className="sales-table">
+            <thead>
+              <tr>
+                <th className="active645">ID</th>
+                <th className="mobile">Data</th>
+                <th>Vendedor</th>
+                <th className="active645">Visitas</th>
+                <th className="active645">Vendas</th>
+                <th>total</th>
+                <th>Notificar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+              <tr>
+                <td className="active645">#001</td>
+                <td className="mobile">20/02/2022</td>
+                <td>Eduardo Marra</td>
+                <td className="active645">20</td>
+                <td className="active645">15</td>
+                <td>563.543,52</td>
+                <td className="notification">
+                  <NotificationButton />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
